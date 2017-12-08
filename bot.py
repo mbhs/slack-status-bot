498f867ee69a76f9b2e08cd688ef1afc2c63c169
Chris Wang <wangch00@icloud.com>
2017-12-07 21:52:47 -0500

    Push some missing changes
    Signed-off-by: Chris Wang <wangch00@icloud.com>

--- bot.py
+++ bot.py
@@ -16,21 +16,20 @@ if __name__ == "__main__":
             if command and channel:
                 handle_command(command, channel)
                 time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+            else:
+                print("Connection failed. Check your Slack token, bot ID, and internet link.")
 
 
 def parse_slack_output(slack_rtm_output):
-        """
-                The Slack Real Time Messaging API is an events firehose.
-                        this parsing function returns None unless a message is
-                                directed at the Bot, based on its ID.
-                                    """
-                                        output_list = slack_rtm_output
-                                            if output_list and len(output_list) > 0:
-                                                        for output in output_list:
-                                                                        if output and 'text' in output and AT_BOT in output['text']:
-                                                                                            # return text after the @ mention, whitespace removed
-                                                                                                            return output['text'].split(AT_BOT)[1].strip().lower(), \
-                                                                                                                                           output['channel']
-                                                                                                                                               return None, None
+    """
+    The Slack Real Time Messaging API is an events firehose.
+    this parsing function returns None unless a message is
+    directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
+    return None, None
